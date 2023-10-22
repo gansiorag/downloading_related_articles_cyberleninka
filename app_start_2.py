aa5d3f02165d7049e70d06bd64ee0c8a2e3e9f29
gansiorag <breslau1962@yandex.ru>
2023-10-22 22:51:01 +0300

    add ch

--- app_start_2.py
+++ app_start_2.py
@@ -1,16 +1,8 @@
 '''
- This module collects images from different sources on request
- Sources:
- 1. https://yandex.ru/images/ - realized
- 2. https://swisscows.com/ru/images?query=фото+пчелы&size=Large
- 2. https://www.google.com/search
- 3. https://images.search.yahoo.com/search/images
- 4. https://duckduckgo.com/
- 5. https://www.startpage.com/sp/search
- 6. https://www.bing.com/images/
+ This module collects 
     
 Athor: Gansior Alexander, gansior@gansior.ru, +79173383804
-Starting 2023/06/20
+Starting 2023/10/10
 Ending 2023//
     
 '''
@@ -19,7 +11,8 @@ from selenium import webdriver
 from time import sleep
 import sys
 import os
-
+from termcolor import cprint
+import inspect
 
 '''
 Text colors: grey red green yellow blue magenta cyan white
@@ -36,29 +29,61 @@ cprint('='*20 + ' >> ' + inspect.stack()[0][0].f_code.co_name + ' << '+'='*20, '
 nameProjectStart = 'downloading_related_articles_cyberleninka'
 file_dir = os.path.dirname(__file__)
 print(file_dir)
-sys.path.append(file_dir.split(nameProjectStart)
-                [0] + nameProjectStart)
+sys.path.append(file_dir.split(nameProjectStart)[0] + nameProjectStart)
 base_path = file_dir.split(nameProjectStart)[0] + nameProjectStart + '/'
 
-driver = webdriver.Chrome('/home/al/Projects_My/downloading_related_articles_cyberleninka/config/chromedriver')
-driver.get('https://cyberleninka.ru')
-in_ask = driver.find_element(By.XPATH, '//fieldset//input')
-fraza = "зрение роботов"
-in_ask.send_keys(f'{fraza}')
-butt = driver.find_element(By.XPATH, '//fieldset//button')
-butt.click()
-first_articles = driver.find_elements(By.XPATH, '//ul[@id="search-results"]//li//h2[@class="title"]')
-print(len(first_articles))
-for el in first_articles:
-    dd = el.find_element(By.XPATH, './a')
-    print(dd.get_attribute("href"))
+from db_modules import WDB
 
-puginators = driver.find_elements(By.XPATH, '//ul[@class="paginator"]//li//a')
-print('puginators = ', len(puginators))
-href4 = puginators[4].get_attribute("href")
-print()
-print(href4)
-sleep(3)
-driver.get(href4)
-sleep(600)
-driver.close()
+def zero_data():
+    data={"questions":"", 
+          "level_link":"", 
+          "href":"", 
+          "file":"", 
+          "name_art":"", 
+          "autor":"", 
+          "year":""}
+    return data    
+
+def step_one(fraza:str):    
+    # start page
+    driver = webdriver.Chrome('/home/al/Projects_My/downloading_related_articles_cyberleninka/config/chromedriver')
+    driver.get('https://cyberleninka.ru')
+    db = WDB('/home/al/Projects_My/downloading_related_articles_cyberleninka/dataset/db_all_info.db')
+
+    # get first page on ask
+    in_ask = driver.find_element(By.XPATH, '//fieldset//input')
+    in_ask.send_keys(f'{fraza}')
+    butt = driver.find_element(By.XPATH, '//fieldset//button')
+    butt.click()
+    sleep(3)
+
+    # get number pagination
+    puginators = driver.find_elements(By.XPATH, '//ul[@class="paginator"]//li//a')
+    print('puginators = ', len(puginators))
+    href_pag_b = puginators[0].get_attribute("href")[:-2]
+    print('href_pag_b == ', href_pag_b)
+    npag = 1
+    while npag <=(len(puginators)+2):
+        
+        # get next partishion articls
+        first_articles = driver.find_elements(By.XPATH, '//ul[@id="search-results"]//li//h2[@class="title"]')
+        print(len(first_articles))
+        for el in first_articles:
+            dd = el.find_element(By.XPATH, './a')
+            print(f'page = {npag} href ==>> {dd.get_attribute("href")}')
+            data_str = zero_data()
+            data_str["href"] = dd.get_attribute("href")
+            data_str["level_link"] = '1'
+            data_str["questions"] = fraza
+            db.add_str_all_info_articles(data_str)
+        # href_pag = puginators[npag].get_attribute("href")
+        print()
+        cprint(f'href_pag_b + str(npag) == {href_pag_b + str(npag)}', 'red', attrs=['bold'])
+        driver.get(href_pag_b + str(npag))
+        npag += 1
+        sleep(3)
+    driver.close()
+
+if __name__ == '__main__':
+    fraza = "зрение роботов"
+    step_one(fraza)
---
+++ db_modules.py
@@ -0,0 +1,54 @@
+'''
+ This module make 
+    
+Athor: Gansior Alexander, gansior@gansior.ru, +79173383804
+Starting 2022//
+Ending 2022//
+    
+'''
+    
+from termcolor import cprint
+import inspect
+'''
+Text colors: grey red green yellow blue magenta cyan white
+Text highlights: on_grey on_red on_green on_yellow on_blue on_magenta on_cyan on_white
+Attributes: bold dark underline blink reverse concealed
+template --> cprint(f'{}' , 'red', attrs=['bold'])
+    
+    
+Shows which module is currently running
+cprint('='*20 + ' >> ' + inspect.stack()[0][0].f_code.co_name + ' << '+'='*20, 'red', attrs=['bold'])
+'''
+    
+import os
+import sys
+import sqlite3
+import json
+
+nameProjectStart = 'downloading_related_articles_cyberleninka'
+file_dir = os.path.dirname(__file__)
+print(file_dir)
+sys.path.append(file_dir.split(nameProjectStart)[0] + nameProjectStart)
+
+
+class WDB():
+    def __init__(self,NameFile:str):
+        self.conn = sqlite3.connect(NameFile, check_same_thread=False)
+        self.cur = self.conn.cursor()
+            
+    def add_str_all_info_articles(self, data: dict):
+        sqlText = 'INSERT INTO all_info_articles (questions, level_link, href, file, name_art, autor, year) '\
+            f"""VALUES('{data["questions"]}', '{data["level_link"]}', '{data["href"]}', '{data["file"]}', """\
+                f"""'{data["name_art"]}', '{data["autor"]}', '{data["year"]}');"""
+        self.cur.execute(sqlText)
+        self.conn.commit()
+    
+    
+def prog2():
+    pass
+    
+    
+if __name__ == '__main__':
+    name=''
+    prog1()
+    prog2()
