00f5f86ae354ca9063e1dad5aebb57acd40814e3
gansiorag <breslau1962@yandex.ru>
2023-11-05 16:55:52 +0300

    add ch

--- col_sek.py
+++ col_sek.py
@@ -7,12 +7,29 @@ Ending 2022//
 
 '''
 
+
+def get_none():
+    """AI is creating summary for get_none
+
+    Returns:
+        [type]: [description]
+    """
+    ff = None
+    return ff
+
+
 if __name__ == '__main__':
-    TIME_1 = '07:04:00'
-    TIME_2 = '08:14:00'
+
+    TIME_1 = '15:42:52'
+    TIME_2 = '19:59:39'
     time_1_list = [int(k) for k in TIME_1.split(':')]
     time_2_list = [int(k) for k in TIME_2.split(':')]
     dd1 = time_1_list[0] * 3600 + time_1_list[1] * 60 + time_1_list[2]
     dd2 = time_2_list[0] * 3600 + time_2_list[1] * 60 + time_2_list[2]
     dr = dd1 - dd2
     print(dr)
+    DD_D = get_none()
+    if DD_D:
+        print(len(DD_D))
+    else:
+        print('None')
